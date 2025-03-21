1ec2c14677b604da29ec0c158cd9e05d71665ba7
tobiasfremming <tobias.fremming@gmail.com>
2025-03-21 12:59:35 +0100

    feat: image extraction

---
+++ src/main.py
@@ -0,0 +1,44 @@
+import PyPDF2
+import os
+
+from PIL import Image
+
+if __name__ == '__main__':
+    
+    # Open the PDF file
+    out_dir = "out"
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+    out_dir += "/"
+    
+    file_name = "domain_repetition.pdf"
+    current_dir = os.path.dirname(__file__) 
+    file_path = os.path.join(current_dir, "pdfs", file_name)
+    input1 = PyPDF2.PdfReader(open(file_path, "rb"))
+    for i in range(len(input1.pages)):
+        page = input1.pages[i]
+        try:
+            xObject = page['/Resources']['/XObject'].get_object()
+        except:
+            continue
+
+        for obj in xObject:
+            if xObject[obj]['/Subtype'] == '/Image':
+                size = (xObject[obj]['/Width'], xObject[obj]['/Height'])
+                data = xObject[obj].get_data()
+                if xObject[obj]['/ColorSpace'] == '/DeviceRGB':
+                    mode = "RGB"
+                else:
+                    mode = "P"
+
+                if xObject[obj]['/Filter'] == '/FlateDecode':
+                    img = Image.frombytes(mode, size, data)
+                    img.save(out_dir+ obj[1:] + ".png")
+                elif xObject[obj]['/Filter'] == '/DCTDecode':
+                    img = open(obj[1:] + ".jpg", "wb")
+                    img.write(out_dir + data)
+                    img.close()
+                elif xObject[obj]['/Filter'] == '/JPXDecode':
+                    img = open(obj[1:] + ".jp2", "wb")
+                    img.write(out_dir + data)
+                    img.close()
